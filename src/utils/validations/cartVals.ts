fa08a819942edae823206ab421274ef08ec872b9
Kodeneko <ladysun.freedom@gmail.com>
2024-09-20 00:00:10 +0200

    [express] validation shippings

--- src/utils/validations/cartVals.ts
+++ src/utils/validations/cartVals.ts
@@ -1,5 +1,7 @@
 import {z} from 'zod';
 import {ArticleValSchema} from './articleVals';
+import ShipStateEnum from '../../model/cart/ShipStateEnum';
+import PaymentEnum from '../../model/cart/PaymentEnum';
 
 const CartLineValSchema = z.object({
   id: z.string(),
@@ -13,6 +15,26 @@ const CartValSchema = z.object({
 });
 const CarIdtValSchema = CartValSchema.extend({id: z.string()});
 
+const ShipStateEnumValSchema = z.nativeEnum(ShipStateEnum);
+const PaymentEnumValSchema = z.nativeEnum(PaymentEnum);
+
+const ShippingValSchema = z.object({
+  id: z.string().nullable(),
+  idTracking: z.string(),
+  idShipping: z.string(),
+  state: z.record(
+    ShipStateEnumValSchema,
+    z.date()
+  ),
+  payment: PaymentEnumValSchema
+});
+
+const ShippingIdValSchema = ShippingValSchema.extend({id: z.string()});
+
 export {
-  CartLineValSchema, CartValSchema, CarIdtValSchema
+  CartLineValSchema,
+  CartValSchema,
+  CarIdtValSchema,
+  ShippingValSchema,
+  ShippingIdValSchema
 };
